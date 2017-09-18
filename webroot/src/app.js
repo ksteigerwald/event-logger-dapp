c62168e600f8ac9dc096e73045db9b5805f24a24
Kris Steigerwald <kris.steigerwald@gmail.com>
2017-09-17 21:53:31 -0400

    -- webpack -- browser ipfs -- web3 connection

--- webroot/src/app.js
+++ webroot/src/app.js
@@ -1,3 +1,16 @@
 import css from './stylesheet.css'
+import Web3 from 'web3'
+import ipfs from 'browser-ipfs'
+
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+ipfs.localProvider = {host: '127.0.0.1', port: '5001', protocol: 'http', root: '/api/v0'};
+
+ipfs.setProvider(ipfs.localProvider)
 
 console.log("Hello from app.js")
+
+ipfs.cat("QmaamdrH6k4KCU4iENDmbZ7WaX93vGwhu8snUeVF2xTMuP", function(err, text) {
+	if (err) throw err;
+	console.log("text:", text); 	// "Testing..."
+});
--- webroot/webpack.config.js
+++ webroot/webpack.config.js
@@ -2,25 +2,31 @@ var path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: 'application.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
-  },
-  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })]
+    entry: './src/app.js',
+    output: {
+        filename: 'application.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ],
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })]
 }
+
